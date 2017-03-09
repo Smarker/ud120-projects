645a96d3788305778b55cbdad4a417a71f61839e
Stephanie Marker <Stephanie.Marker93@gmail.com>
2017-03-09 15:18:17 -0500

    practice kmeans clustering with different num clusters

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,49 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+#person who made the most at ENRON:
+"""
+guy = ""
+money = 0
+for person in enron_data:
+    if isinstance( enron_data[person]["total_payments"], ( int ) ) and person != "TOTAL":
+        if enron_data[person]["total_payments"] > money:
+            money = enron_data[person]["total_payments"]
+            guy = person
+print guy
+print money
+"""
+
+#people with salary and emails that exist
+"""
+num_salary = 0;
+num_email = 0;
+for person in enron_data:
+    if isinstance( enron_data[person]["salary"], ( int ) ):
+        num_salary = num_salary + 1
+    if enron_data[person]["email_address"] != "NaN":
+        num_email = num_email + 1
+print num_salary
+print num_email
+"""
+
+# % of POIs with NaN as total_payments
+num_NaN = 0
+num_poi = 0
+for person in enron_data:
+    if enron_data[person]["poi"]:
+        num_poi = num_poi + 1
+        if enron_data[person]["total_payments"] == "NaN":
+            num_NaN = num_NaN + 1
+print num_poi
+print float(num_NaN)/float(num_poi)
+
+#number of people with NaN as total payments
+num_NaN = 0
+for person in enron_data:
+    if enron_data[person]["total_payments"] == "NaN":
+        num_NaN = num_NaN + 1
+print float(num_NaN)
+
+print len(enron_data.keys())
 
